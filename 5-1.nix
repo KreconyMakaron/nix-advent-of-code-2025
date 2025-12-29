f999772c12d91abb9589ad5b32f79d9b8575ac31
KreconyMakaron <55319736+KreconyMakaron@users.noreply.github.com>
2025-12-29 20:01:21 +0100

    solve 5

---
+++ 5-1.nix
@@ -0,0 +1,15 @@
+let
+  lib = import <nixpkgs/lib>;
+in
+  with lib; let
+    lines = filter (x: x != "") (splitString "\n" (builtins.readFile ./input/5.txt));
+
+    isRange = x: elem "-" (stringToCharacters x);
+
+    ranges = map (x: map toInt (splitString "-" x)) (filter isRange lines);
+    ids = map toInt (filter (x: !(isRange x)) lines);
+
+    isInRange = x: any (y: (head y) <= x && x <= (last y)) ranges;
+    fresh = filter isInRange ids;
+  in
+    length fresh
